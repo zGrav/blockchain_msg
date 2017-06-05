642b6425b9697cb84a742a1ceaaf65e113a42094
David Silva <zgrav@null.net>
2017-06-05 12:13:20 +0200

    comment out console.logs

--- app/worker.js
+++ app/worker.js
@@ -8,7 +8,7 @@ function work(block, difficulty) {
 		prefix += '0';
 	}
 
-	console.log("worker difficulty prefix is " + prefix);
+	//console.log("worker difficulty prefix is " + prefix);
 
 	/* Adjust difficulty based on amount of messages. */
 	while (!block.hash.startsWith(prefix)) {
@@ -24,28 +24,28 @@ function work(block, difficulty) {
 		counter++;
 
 		if (counter % 1000 === 0) {
-			console.log("worker sha256 counter is " + counter);
+			//console.log("worker sha256 counter is " + counter);
 		}
 	}
 
-	console.log("created block: " + JSON.stringify(block));
+	//console.log("created block: " + JSON.stringify(block));
 
 	return block;
 }
 
 /* callback from main script */
 onmessage = function(e) {
-	console.log('Message received from main script');
+	//console.log('Message received from main script');
 
 	const block = e.data[0];
 	const difficulty = e.data[1];
 
-	console.log("worker:onmessage block is " + JSON.stringify(block));
-	console.log("worker:onmessage difficulty is " + JSON.stringify(difficulty));
+	//console.log("worker:onmessage block is " + JSON.stringify(block));
+	//console.log("worker:onmessage difficulty is " + JSON.stringify(difficulty));
 
 	const result = work(block, difficulty);
 
-	console.log("worker found a result: " + JSON.stringify(block));
+	//console.log("worker found a result: " + JSON.stringify(block));
 
 	postMessage(result);
 };
