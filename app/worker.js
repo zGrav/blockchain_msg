83df48cfe6ebcc740c493744a69fb74feceabd51
David Silva <zgrav@null.net>
2017-06-05 11:22:53 +0200

    add server/public/app

---
+++ app/worker.js
@@ -0,0 +1,42 @@
+let CryptoJS = require('crypto-js');
+
+function work(block, difficulty) {
+  let counter = 0;
+  let prefix = "";
+  for(var i = 0; i < difficulty; i++)
+    prefix += "0";
+  console.log("worker difficulty prefix is " + prefix);
+
+  /* Adjust difficulty based on amount of messages. */
+  while(!block.hash.startsWith(prefix))
+  {
+    /* nonce is about to flip over, update timestamp and start again. */
+    if(block.header.nonce == Number.MAX_VALUE - 1)
+    {
+      block.header.nonce = Number.MIN_VALUE;
+      block.header.timestamp = new Date().valueOf();
+    }
+
+    block.header.nonce++;
+    block.hash = CryptoJS.SHA256(CryptoJS.SHA256(JSON.stringify(block))).toString(CryptoJS.enc.Base64);
+
+    counter++;
+    if(counter % 1000 == 0)
+      console.log("worker sha256 counter is " + counter);
+  }
+
+  console.log("created block: " + JSON.stringify(block));
+  return block;
+}
+
+/* callback from main script */
+onmessage = function(e) {
+  console.log('Message received from main script');
+  let block = e.data[0];
+  let difficulty = e.data[1];
+  console.log("worker:onmessage block is " + JSON.stringify(block));
+  console.log("worker:onmessage difficulty is " + JSON.stringify(difficulty));
+  let result = work(block, difficulty);
+  console.log("worker found a result: " + JSON.stringify(block));
+  postMessage(result);
+}
---
+++ server/index.js
@@ -0,0 +1,134 @@
+let express = require('express');
+let app = express();
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
+let crypto = require('crypto');
+let CryptoJS = require('crypto-js');
+
+app.use(express.static('/public'));
+app.use(express.static('/app'));
+
+let randomGenesisHash = "";
+
+let current_date = (new Date()).valueOf().toString();
+let random = Math.random().toString();
+
+randomGenesisHash = crypto.createHash('sha1').update(current_date + random).digest('hex');
+randomGenesisHash = CryptoJS.SHA256(CryptoJS.SHA256(randomGenesisHash)).toString(CryptoJS.enc.Base64);
+
+let genesis = {
+  hash: randomGenesisHash,
+  header: {
+    index: 0,
+    previousHash: "",
+    timestamp: Math.floor(Date.now() / 1000),
+    nonce: 0
+  },
+  data: {
+    sender: "zgrav",
+    message: "time has begun flowing.",
+  }
+};
+
+let difficulty = 1;
+let users = [];
+let blocks = [ genesis ];
+let nBlocks = blocks.length;
+/*
+   Adjust difficulty as follows:
+   difficulty = number of received blocks during last minute (+ 1)
+*/
+setInterval(function() {
+  difficulty = (blocks.length - nBlocks) + 1;
+  nBlocks = blocks.length;
+  console.log("Difficulty during previous 15 seconds: " +difficulty);
+  io.emit('set-difficulty', difficulty);
+}, 15000);
+
+function getPlainUsersArray(users) {
+    let output = [];
+
+    for(var i = 0; i < users.length; i++) {
+      let user = {};
+      user['id'] = users[i].id;
+      user['address'] = users[i].request.connection._peername.address;
+      user['family'] = users[i].request.connection._peername.family;
+      user['port'] = users[i].request.connection._peername.port;
+      output.push(user);
+    }
+
+    console.log("getPlainUsersArray(): " + JSON.stringify(output));
+    return output;
+}
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/blockchain', function(req, res){
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(blocks));
+});
+
+io.on('connection', function(socket){
+
+  /* Upon receiving a new connection, inform the client of its identity. */
+  socket.emit('set-identity', socket.id);
+
+  /* Upon receiving a new connection, update the list of clients and send it to all clients. */
+  users.push(socket);
+  io.emit('set-peers', getPlainUsersArray(users));
+
+  socket.on('get-peers', function() {
+    socket.emit('set-peers', getPlainUsersArray(users));
+  });
+
+  socket.on('get-difficulty', function() {
+    socket.emit('set-difficulty', difficulty);
+  });
+
+  socket.on('get-blocks', function() {
+    socket.emit('set-blocks', blocks);
+  });
+
+  socket.on('new-block', function(block) {
+    let lastBlock = blocks[blocks.length - 1];
+
+    /* validate block */
+
+    /* validate sender id */
+    if(block.data.sender.valueOf() == socket.id.valueOf())
+      console.log("sender id matches");
+    else
+      console.log("sender id does not match");
+
+    /* validate index */
+    if(block.header.index == lastBlock.header.index + 1)
+      console.log("index matches");
+    else
+      console.log("index does not match");
+
+    /* validate hash */
+    if(block.header.previousHash.valueOf() == lastBlock.hash.valueOf())
+      console.log("previousHash matches");
+    else
+      console.log("previousHash does not match");
+
+    /* JSON.parse(block); */
+    console.log("on block: " + JSON.stringify(block));
+    blocks.push(block);
+    io.emit('new-block', block);
+
+    difficulty++;
+    io.emit('set-difficulty', difficulty);
+  });
+
+  socket.on('disconnect', function() {
+    users = users.filter(function(el) { return el.id != socket.id; });
+    io.emit('users', getPlainUsersArray(users));
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
